30201b01fe1baa2c8934138edeefbc452a48b1f1
Tanishkajagtap12 <127036416+Tanishkajagtap12@users.noreply.github.com>
2023-12-15 12:54:16 +0530

    Add files via upload

---
+++ Schaeffler_frontend/admin-panel.js
@@ -0,0 +1,8 @@
+function loadContent(url) {
+    // Get the iframe element
+    var iframe = document.getElementById("contentFrame");
+  
+    // Set the src attribute to load the specified URL
+    iframe.src = url;
+  }
+  
